60670d6ba78a341e35dcbcb7bb050ecf5b80f6a6
Jhonny2809 <63829112+Jhonny2809@users.noreply.github.com>
2025-12-22 19:06:44 -0300

    Update App.tsx
    Alterei para BrowserRouter

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import { SiteProvider } from './context/SiteContext';
